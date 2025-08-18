0d7ffbfc88521f280242f258eb0dc68f4ce8fb6a
rockefelm <rockefelm@gmail.com>
2025-08-18 14:18:43 -0400

    refactored the validation check to use express.json instead of manually parsing it.

--- src/api/validatechirp.ts
+++ src/api/validatechirp.ts
@@ -1,23 +1,13 @@
 import {Request, Response} from "express";
 
 export async function handlerValidateChirp(req: Request, res: Response) {
-    let body = ""; // Initializer
-
-    req.on("data", (item) => {
-        body += item;
-    }); // listens for data events and puts them and adds them to the body.
-
-    req.on("end", () => {
-        try {
-            const parsedBody = JSON.parse(body); 
-            if (parsedBody.body.length <= 140) {
-                res.send({ "valid": true });
+    try { 
+        if (req.body.body.length <= 140) {
+            res.send({ "valid": true });
             } else {
                 res.status(400).send({ "error": "Chirp is too long"});
             }
         } catch (error) {
             res.status(400).send({ error: "Something went wrong" });
         }
-    }); /* parses the body into a JSON blob and returns the 
-        appropriate response based on the length of the chirp. */
 }
