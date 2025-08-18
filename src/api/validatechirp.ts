031d9054e73056e53fdf3580bbc08f9cc5483139
rockefelm <rockefelm@gmail.com>
2025-08-18 14:45:32 -0400

    added profanity filter to handlerValidateChirp thats scrubs profanity from the post and returns a cleaned JSON blob.

--- src/api/validatechirp.ts
+++ src/api/validatechirp.ts
@@ -1,13 +1,22 @@
 import {Request, Response} from "express";
 
 export async function handlerValidateChirp(req: Request, res: Response) {
-    try { 
-        if (req.body.body.length <= 140) {
-            res.send({ "valid": true });
-            } else {
-                res.status(400).send({ "error": "Chirp is too long"});
-            }
-        } catch (error) {
-            res.status(400).send({ error: "Something went wrong" });
+    if (req.body.body.length > 140) {
+        res.status(400).send({ "error": "Chirp is too long"});
+        return;
+    } 
+
+    const words = req.body.body.split(" ");
+
+    const badWords = ["kerfuffle", "sharbert", "fornax"];
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i]
+        const lowerWord = word.toLowerCase();
+        if (badWords.includes(lowerWord)) {
+            words[i] = "****";
         }
+    }
+
+    const filtered = words.join(" ");
+    res.send({ cleanedBody: filtered });
 }
