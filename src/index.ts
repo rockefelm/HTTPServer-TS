b469d8f96d96bf7ca55eef57d3936313df58d759
rockefelm <rockefelm@gmail.com>
2025-08-18 14:07:41 -0400

    setup a post to test if a chirps body is 140 or less characters and throw errors accordingly.

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { middlewareLogResponses, middlewareMetricsInc } from "./api/middleware.js";
 import { handlerServerHits, handlerServerHitsReset } from "./api/serverhits.js";
+import { handlerValidateChirp } from "./api/validatechirp.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static('./src/app'));
+app.use(express.json());
 app.get("/admin/metrics", handlerServerHits);
-app.get("/admin/reset", handlerServerHitsReset);
+app.post("/admin/reset", handlerServerHitsReset);
+app.post("/api/validate_chirp", handlerValidateChirp);
 app.get("/api/healthz", handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
