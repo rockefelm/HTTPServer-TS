913b8a2793daa5ab96f8cf8fb08c51b653830d27
rockefelm <rockefelm@gmail.com>
2025-08-18 23:28:35 -0400

    refactored the dbURL string into the .env and .gitignored it for safety also added a createUser query .

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "./src/config.js";
 
 export default defineConfig({
   schema: "src/db/schema.ts",
   out: "src/db",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgres://postgres:postgres@localhost:5432/chirpy?sslmode=disable",
+    url: config.dbURL
   },
 });
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
+import { loadEnvFile } from 'node:process';
+loadEnvFile();
+
 type APIConfig = {
     fileServerHits: number;
+    dbURL: string;
 };
 
 export const config: APIConfig = {
-    fileServerHits: 0
+    fileServerHits: 0,
+    dbURL: envOrThrow("DB_URL")
+}
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is required`);
+  }
+  return value;
 }
